f8977f2cc98b061120d8ee79c71972f7cf46927c
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-01-06 13:58:39 -0600

    Update Solution.cpp

--- Learn C- Hello World/Hello World/5. Review/Solution.cpp
+++ Learn C- Hello World/Hello World/5. Review/Solution.cpp
@@ -4,7 +4,7 @@ int main()
 {
   
   std::cout << "Dear Self,\n";
-  std::cout << "graduate college please!!\n";
+  std::cout << "graduate from college please!!\n";
   std::cout << "December 05, 2018\n";
   
 }
